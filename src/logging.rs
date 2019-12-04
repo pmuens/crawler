89ede4ce875c587f9a6ded72f13e46311631a2bf
Philipp Muens <raute1337@gmx.de>
2019-12-04 16:22:53 +0100

    Simple logging capabilities

---
+++ src/logging.rs
@@ -0,0 +1,24 @@
+extern crate chrono;
+
+use self::chrono::Utc;
+use chrono::DateTime;
+use std::time::SystemTime;
+
+pub enum Log {
+    Info(&'static str),
+    Warn(&'static str),
+    Fatal(&'static str),
+}
+
+impl Log {
+    pub fn print(&self) {
+        let system_time = SystemTime::now();
+        let date_time: DateTime<Utc> = system_time.into();
+        let time_formatted = date_time.format("%d/%m/%Y %T");
+        match *self {
+            Log::Info(msg) => println!("INFO - {} - \"{}\"", time_formatted, msg),
+            Log::Warn(msg) => println!("WARN - {} - \"{}\"", time_formatted, msg),
+            Log::Fatal(msg) => eprintln!("FATAL - {} - \"{}\"", time_formatted, msg),
+        }
+    }
+}
