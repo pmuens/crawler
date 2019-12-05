78a25404dda3d13dea21d842353c0f500317746f
Philipp Muens <raute1337@gmx.de>
2019-12-05 15:49:44 +0100

    Use Strings in loggings

--- src/logging.rs
+++ src/logging.rs
@@ -5,9 +5,9 @@ use chrono::DateTime;
 use std::time::SystemTime;
 
 pub enum Log {
-    Info(&'static str),
-    Warn(&'static str),
-    Fatal(&'static str),
+    Info(String),
+    Warn(String),
+    Fatal(String),
 }
 
 impl Log {
@@ -15,7 +15,7 @@ impl Log {
         let system_time = SystemTime::now();
         let date_time: DateTime<Utc> = system_time.into();
         let time_formatted = date_time.format("%d/%m/%Y %T");
-        match *self {
+        match self {
             Log::Info(msg) => println!("INFO - {} - \"{}\"", time_formatted, msg),
             Log::Warn(msg) => println!("WARN - {} - \"{}\"", time_formatted, msg),
             Log::Fatal(msg) => eprintln!("FATAL - {} - \"{}\"", time_formatted, msg),
