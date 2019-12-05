0fd1d53f46be3844cacc85f719632487e5d457ba
Philipp Muens <raute1337@gmx.de>
2019-12-05 21:17:08 +0100

    HashSet for JobQueue / ProcessedJobs / Job updates

--- src/job.rs
+++ src/job.rs
@@ -1,80 +1,85 @@
-use std::cmp::Ordering;
-use std::collections::BinaryHeap;
+use std::collections::HashSet;
 use std::str::FromStr;
 use url::Url;
 
-/// Priority (P1 = Highest)
-#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
-#[allow(dead_code)]
-pub enum Priority {
-    P1 = 0,
-    P2 = 1,
-    P3 = 2,
-    P4 = 3,
+// TODO: this set needs to be cleaned-up on a recurring basis to reclaim memory during runtime
+pub type ProcessedJobs = HashSet<Job>;
+pub type JobQueue = HashSet<Job>;
+
+pub trait Queue<T> {
+    fn enqueue(&mut self, value: T);
+    fn dequeue(&mut self) -> Option<T>;
 }
 
-/// BinaryHeap which sorts Jobs to be processed based on their Priority
-#[allow(dead_code)]
-pub type JobQueue<Job> = BinaryHeap<Job>;
-
-/// A crawling job
-#[derive(Debug)]
-pub struct Job {
-    pub priority: Priority,
-    pub url: Url,
-}
-
-impl Job {
-    pub fn new(url: Url, priority: Priority) -> Self {
-        Job { url, priority }
+// NOTE: this implementation isn't respecting the ordering of a traditional Queue
+// as we're dealing with a HashSet here which doesn't guarantee ordering
+impl Queue<Job> for JobQueue {
+    fn enqueue(&mut self, value: Job) {
+        self.insert(value);
     }
-}
 
-impl Ord for Job {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.priority.cmp(&other.priority)
-    }
-}
-
-impl PartialOrd for Job {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        if self == other {
-            Some(Ordering::Equal)
-        } else if self.priority <= other.priority {
-            Some(Ordering::Greater)
-        } else if self.priority >= other.priority {
-            Some(Ordering::Less)
-        } else {
-            None
+    fn dequeue(&mut self) -> Option<Job> {
+        if self.is_empty() {
+            return None;
         }
-    }
-}
-
-impl Eq for Job {}
-
-impl PartialEq for Job {
-    fn eq(&self, other: &Self) -> bool {
-        self.priority == other.priority
+        // NOTE: we already checked if there are some values so we can safely
+        // unwrap the Option here
+        let value = self.iter().next().unwrap().to_owned();
+        if !self.remove(&value) {
+            return None;
+        }
+        Some(value)
     }
 }
 
 #[test]
-fn job_queue() {
+fn job_queue_enqueue() {
     let to_url = |url: &str| Url::from_str(url).unwrap();
+    let mut queue = JobQueue::new();
 
-    let job1 = Job::new(to_url("https://example.com/p1/1"), Priority::P1);
-    let job2 = Job::new(to_url("https://example.com/p1/2"), Priority::P1);
-    let job3 = Job::new(to_url("https://example.com/p2/1"), Priority::P2);
-    let job4 = Job::new(to_url("https://example.com/p2/2"), Priority::P2);
-    let job5 = Job::new(to_url("https://example.com/p3/1"), Priority::P3);
+    let job_1 = Job::new(to_url("http://example-1.com"));
+    let job_2 = Job::new(to_url("http://example-1.com"));
+    let job_3 = Job::new(to_url("http://example-2.com"));
+    let job_4 = Job::new(to_url("http://example-2.com"));
 
-    let jobs = vec![&job2, &job5, &job3, &job4, &job1];
+    queue.enqueue(job_1);
+    queue.enqueue(job_2);
+    queue.enqueue(job_3);
+    queue.enqueue(job_4);
 
-    let mut queue = JobQueue::from(jobs);
+    assert_eq!(queue.len(), 2);
+    assert!(queue.contains(&Job::new(to_url("http://example-1.com"))));
+    assert!(queue.contains(&Job::new(to_url("http://example-2.com"))));
+}
 
-    assert_eq!(queue.pop(), Some(&job1));
-    assert_eq!(queue.pop(), Some(&job2));
-    assert_eq!(queue.pop(), Some(&job3));
-    assert_eq!(queue.pop(), Some(&job4));
-    assert_eq!(queue.pop(), Some(&job5));
+#[test]
+fn job_queue_dequeue() {
+    let to_url = |url: &str| Url::from_str(url).unwrap();
+    let mut queue = JobQueue::new();
+
+    let job_1 = Job::new(to_url("http://example.com"));
+
+    queue.enqueue(job_1);
+
+    assert_eq!(
+        queue.dequeue(),
+        Some(Job::new(to_url("http://example.com")))
+    );
+    assert_eq!(queue.dequeue(), None);
+}
+
+/// A crawling job
+#[derive(Eq, PartialEq, Hash, Clone, Debug)]
+pub struct Job {
+    url: Url,
+}
+
+impl Job {
+    pub fn new(url: Url) -> Self {
+        Job { url }
+    }
+
+    pub fn get_url(&self) -> Url {
+        self.url.to_owned()
+    }
 }
