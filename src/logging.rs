3c11bd21c85175ae95a850c294ba0f71fde97702
Philipp Muens <raute1337@gmx.de>
2019-12-07 22:03:56 +0100

    Implement Display for easier Log usage

--- src/logging.rs
+++ src/logging.rs
@@ -1,5 +1,6 @@
 use chrono::DateTime;
 use chrono::Utc;
+use std::fmt::{self, Display};
 use std::time::SystemTime;
 
 pub enum Log {
@@ -8,15 +9,26 @@ pub enum Log {
     Fatal(String),
 }
 
-impl Log {
-    pub fn print(&self) {
+impl Display for Log {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let system_time = SystemTime::now();
         let date_time: DateTime<Utc> = system_time.into();
         let time_formatted = date_time.format("%d/%m/%Y %T");
-        match self {
-            Log::Info(msg) => println!("INFO - {} - \"{}\"", time_formatted, msg),
-            Log::Warn(msg) => println!("WARN - {} - \"{}\"", time_formatted, msg),
-            Log::Fatal(msg) => eprintln!("FATAL - {} - \"{}\"", time_formatted, msg),
-        }
+        let msg = match self {
+            Log::Info(msg) => format!("INFO - {} - \"{}\"", time_formatted, msg),
+            Log::Warn(msg) => format!("WARN - {} - \"{}\"", time_formatted, msg),
+            Log::Fatal(msg) => format!("FATAL - {} - \"{}\"", time_formatted, msg),
+        };
+        write!(f, "{}", msg)
     }
+}
+
+#[test]
+fn log_types() {
+    let info = Log::Info("Foo".to_string()).to_string();
+    assert!(info.starts_with("INFO - "));
+    let warn = Log::Warn("Foo".to_string()).to_string();
+    assert!(warn.starts_with("WARN - "));
+    let fatal = Log::Fatal("Foo".to_string()).to_string();
+    assert!(fatal.starts_with("FATAL - "));
 }
